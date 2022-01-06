5c16847ea6b433fa0e98c697fafbcb8eb00a4424
gabrielsterponemagni <94461239+gabrielsterponemagni@users.noreply.github.com>
2022-01-06 12:19:37 +0000

    insert item into an array

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ export function addToBeginning(array, item) {
 }
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+  const originalArray = [...array.slice(0, index), item, ...array.slice(index)]
+  return originalArray;
+}
 
 // Immutably replace an item at a specific position/index within an array
 export function replaceItem(array, item, index) {}
